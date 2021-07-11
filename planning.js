b7912ee56cb6ccabfd65f82012fda41b33e8bfe5
HarikrishnanKNair <harikrishnan2112003@gmail.com>
2021-07-11 13:16:30 +0530

    first commit

---
+++ planning.js
@@ -0,0 +1,29 @@
+/*
+-> We need a reusable carousel
+    ->Hero Carousel
+    ->Common Carousel
+    ->Cast and Crew small Carousel
+
+->Pages
+    ->Home Page
+    ->Movie Page
+    ->Category Page
+    ->Auth Page
+    ->Razorpay Payment Gateway
+
+->Plan
+    ->Layouts
+    ->Routes
+    ->Pages
+    ->Components
+
+::What is HOC?
+  ->Higher Order Components
+::Layout First Approach?
+  ->Default Layout
+  ->Layout will not render everytime i.e. 
+    Navbar and Footer will not be changed 
+    and rendered again and again.Only 
+    body will be updated with the changes.
+
+*/
